ee7446c37ec62ef6335b26be8d84f11fc8b717e0
ismailessaouabi <ismailessaouabi63@gmail.com>
2025-11-17 08:33:33 +0100

    ...

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import Header from "../paretails/header";
+import Footer from "../paretails/footer";
 
 export default function Home() {
     return (
-        <div className="home">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main landing page of the application.</p>
-        <Link to="/register">Go to Register Page</Link>
-        </div>
+        <>
+            <Header/>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main landing page of the application.</p>
+            <Link to="/register">Go to Register Page</Link>
+            <Footer/>
+        </>
     )
     }
