1fe56aa519fe749effd8746c8236fc265f5c561c
ismailessaouabi <ismailessaouabi63@gmail.com>
2025-11-17 08:28:27 +0100

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import Register from "./pages/register"
 import Profile from "./pages/profile"
 import { BrowserRouter, Routes ,Route } from "react-router-dom"
 import Login from "./pages/login"
+import Home from "./pages/home"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
+        <div className="home">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main landing page of the application.</p>
+        <Link to="/register">Go to Register Page</Link>
+        </div>
+    )
+    }
